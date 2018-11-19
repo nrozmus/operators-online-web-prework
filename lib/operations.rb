77f1730bdfe18cc258ab63f85470adf49c06dd5f
Nicholas Rozmus <rozmusnicholas@gmail.com>
2018-11-19 22:38:52 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,14 +1,13 @@
 def unsafe?(speed)
-if unsafe? > 60 
-"true"
-else "false"
+require 'pry' 
+ def unsafe?(speed)
+  if 40 > speed || speed > 60
+    return true
+  else
+    return false
+  end
 end
-
-
-
-def not_safe?(speed)
+ def not_safe?(speed)
 	
+  ((speed > 60) || (speed < 40))? true : false
 end
-	
-
-
